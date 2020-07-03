293754597027d454fdeae766c96b92fcec9a676a
JonatasHdS <61765878+JonatasHdS@users.noreply.github.com>
2020-07-03 15:20:27 -0300

    Update Tempo_Hashtag.py
    Adicionado mais parametros no csv e um contador de posts coletados

--- Tempo_Hashtag.py
+++ Tempo_Hashtag.py
@@ -5,6 +5,7 @@ def ColetaHashtag (loader):
 		# Coleta info de uma hashtag
 	
 		tag = str(input("Digite a palavra que deve ser buscada: ")) #Faz a coleta de qual hashtag deve ser buscada
+		cont_max = int(input("Quantos posts devem ser buscados: "))
 		a,m,d = input("Digite uma data para ser o inicio da pesquisa(aaaa-mm-dd): ").split("-")
 		data_ini = datetime.datetime(int(a),int(m),int(d))
 		a,m,d = input("Digite uma data para ser o fim da pesquisa(aaaa-mm-dd): ").split("-")
@@ -15,13 +16,17 @@ def ColetaHashtag (loader):
 		print('Posts count: ' + str(hashtag.mediacount))
 		print('\n')
 		#Faz download dos posts associados com a hashtag
-		with open(tag+'.csv', 'w', newline='') as file:
+		cont = 0
+		with open(tag+'.csv', 'w', encoding='utf-8', newline='') as file:
 			writer = csv.writer(file)
-			writer.writerow(["Likes", "Comentários", ""])
+			writer.writerow(["Usuario", "Data", "Likes", "Comentarios", "Texto", "Hashtags", "Patrocinado"])
 			for post in hashtag.get_posts():
 				if (post.date <= data_ini) and (post.date >= data_fin): #Verifica se o post está na margem de tempo desejada
 					print(post.date)
-					writer.writerow([post.likes, post.comments, post])
-				#loader.download_post(post, target="#"+hashtag.name) #Faz o download dos itens do post
-			#elif post.date  < data_fin: #Caso não haja mais posts neste mês finaliza
-				#print("finalizado" + teste) 
+					loader.download_post(post, target="#"+hashtag.name) #Faz o download dos itens do post
+					writer.writerow([post.owner_username, post.date, post.likes, post.comments, post.caption, post.caption_hashtags, post.is_sponsored]) #Coleta os dados referentes as colunas do arquivo csv
+					cont += 1
+				if cont == cont_max:
+					print("Finalizado!!")
+					break
+				
