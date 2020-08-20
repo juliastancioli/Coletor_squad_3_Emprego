6627d8b7cddb993f57ac00d1b8ee16447adcded6
Vinicius Correia Fonseca de Castro <vinicius.correiacastro@gmail.com>
2020-08-20 12:06:34 -0300

    Update Subtarefas_Modulo.py

--- Subtarefas_Modulo.py
+++ Subtarefas_Modulo.py
@@ -25,23 +25,31 @@ def kmp(t, p):
 
 # Função que a partir dos dados de longitude e latitude busca a localização do post caso esteja disponivel
 def localiza(lat, long):
-    geolocator = Nominatim(user_agent="CDA UFMG")
-    loc = str(lat)+", "+str(long)
-    location = geolocator.reverse(loc)
-    dados = []
-    if "state" in location.raw["address"].keys():
-        dados.append(location.raw["address"]["state"])
-    else:
-        dados.append("None")
-    if "city" in location.raw["address"].keys():
-        dados.append(location.raw["address"]["city"])
-    else:
-        dados.append("None")
-    if "region" in location.raw["address"].keys():
-        dados.append(location.raw["address"]["region"])
-    else:
-        dados.append("None")
-    return dados
+	geolocator = Nominatim(user_agent="CDA UFMG")
+	loc = str(lat)+", "+str(long)
+	location = geolocator.reverse(loc)
+	dados = []
+	if "state" in location.raw["address"].keys():
+		dados.append(location.raw["address"]["state"])
+	else:
+		dados.append("None")
+	if "city" in location.raw["address"].keys():
+		cidade = str(location.raw["address"]["city"])
+		dados.append(cidade)
+		cidade = cidade.replace(" ", "").replace("á", "a").replace("ã", "a").replace("â", "a").replace("é", "e").replace("ê", "e").replace("î", "i").replace("í", "i").replace("õ", "o").replace("ô", "o").replace("ó", "o")
+		f = open("cidades.txt", "r")
+		if kmp(f.readline(),cidade.lower()) != []:
+			dados.append(True)
+		else:
+			dados.append(False)
+	else:
+		dados.append("None")
+		dados.append("None")
+	if "region" in location.raw["address"].keys():
+		dados.append(location.raw["address"]["region"])
+	else:
+		dados.append("None")
+	return dados
 
 
 
